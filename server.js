2ec40c311e26b19431b2a83d159bd95275115a7e
vidit-agarwal <viditvivo@gmail.com>
2018-07-21 01:13:45 +0530

    Finishing Up

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ mongo.connect('mongodb://127.0.0.1/chat' , function(err , db){
     if(err) throw err ;
 
     client.on('connection' , function(socket){
-<<<<<<< HEAD
+
         var col = db.collection('messages') ,
             sendStatus = function(s)
             {   
@@ -21,33 +21,24 @@ mongo.connect('mongodb://127.0.0.1/chat' , function(err , db){
                 if(err) throw err ;
                 socket.emit('output' , res) ;
             }) ;
-=======
-        var col = db.collection('messages') ;
->>>>>>> f3dcf78e64d8d6ba03d7771b5b32c8f7c807fd5d
+
 
         //wait for input
 
         socket.on('input' , function(data){
             var name = data.name ,
                 message = data.message , 
-<<<<<<< HEAD
+
                     whitespacePattern = /^\s*$/ ; 
 
             if(whitespacePattern.test(name) || whitespacePattern.test(message))
             {
                 sendStatus('Name Or message is required') ;
-=======
-                    whitespacePattern = /^\s*$/ ;
-
-            if(whitespacePattern.test(name) || whitespacePattern.test(message))
-            {
-                console.log('Invalid') ;
->>>>>>> f3dcf78e64d8d6ba03d7771b5b32c8f7c807fd5d
             }
             else{
                 col.insert({name : name , message:message} , function()
                 {
-<<<<<<< HEAD
+
 
                     //Emit latest message
 
@@ -57,9 +48,8 @@ mongo.connect('mongodb://127.0.0.1/chat' , function(err , db){
                         message : "Message send" ,
                         clear : true 
                     }) ;
-=======
                     console.log('INserted') ;
->>>>>>> f3dcf78e64d8d6ba03d7771b5b32c8f7c807fd5d
+
                 });
             }
         })
