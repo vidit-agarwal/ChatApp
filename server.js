f3dcf78e64d8d6ba03d7771b5b32c8f7c807fd5d
vidit-agarwal <viditvivo@gmail.com>
2018-07-20 23:51:23 +0530

    Updating socket.io code

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log('worked') ;
+var mongo = require('mongodb').MongoClient ;
+var client = require('socket.io').listen(8080 , {origins: '*:*'}).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat' , function(err , db){
+
+    if(err) throw err ;
+
+    client.on('connection' , function(socket){
+        var col = db.collection('messages') ;
+
+        //wait for input
+
+        socket.on('input' , function(data){
+            var name = data.name ,
+                message = data.message , 
+                    whitespacePattern = /^\s*$/ ;
+
+            if(whitespacePattern.test(name) || whitespacePattern.test(message))
+            {
+                console.log('Invalid') ;
+            }
+            else{
+                col.insert({name : name , message:message} , function()
+                {
+                    console.log('INserted') ;
+                });
+            }
+        })
+    });
+});
